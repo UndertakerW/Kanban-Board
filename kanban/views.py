ed9061c9a14e16a5d929bb8d350358824cc3f0ef
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2023-03-19 18:26:53 -0400

    Add register_action

--- kanban/views.py
+++ kanban/views.py
@@ -2,8 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.models import User
-from kanban.forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login, logout
+
+from kanban.forms import LoginForm, RegisterForm
+from kanban.models import Profile
 
 # Function name:    compute_context
 # url:
@@ -72,8 +74,39 @@ def logout_action(request):
 
 
 def register_action(request):
-    context = {}
-    render(request, "kanban/register.html", context)
+    context = compute_context(request)
+
+    # Just display the registration form if this is a GET request.
+    if request.method == 'GET':
+        context['form'] = RegisterForm()
+        return render(request, 'kanban/register.html', context)
+
+    # Creates a bound form from the request POST parameters and makes the
+    # form available in the request context dictionary.
+    form = RegisterForm(request.POST)
+    context['form'] = form
+
+    # Validates the form.
+    if not form.is_valid():
+        return render(request, 'kanban/register.html', context)
+
+    # At this point, the form data is valid.  Register and login the user.
+    new_user = User.objects.create_user(username=form.cleaned_data['username'],
+                                        password=form.cleaned_data['password1'],
+                                        email=form.cleaned_data['email'],
+                                        first_name=form.cleaned_data['first_name'],
+                                        last_name=form.cleaned_data['last_name'])
+    new_user.save()
+
+    new_user = authenticate(username=form.cleaned_data['username'],
+                            password=form.cleaned_data['password1'])
+
+    profile = Profile()
+    profile.user = new_user
+    profile.save()
+
+    login(request, new_user)
+    return redirect(reverse('home'))
 
 
 def create_workspace_action(request):
