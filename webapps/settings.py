8b34daa966e4ca78f9fab4122c3f6d3f1c839b7c
Minhui Xie <minhuix@andrew.cmu.edu>
2023-03-21 21:42:24 -0400

    built workspace screen with task and board modals

--- webapps/settings.py
+++ webapps/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kanban',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 ROOT_URLCONF = 'webapps.urls'
 
