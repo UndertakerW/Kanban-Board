26ed5610b56e1c8988b45ff572bc721cf57936cd
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2023-04-13 02:29:52 -0400

    Update routing for ws

--- kanban/routing.py
+++ kanban/routing.py
@@ -2,5 +2,5 @@ from django.urls import path, path
 from kanban import consumers
 
 websocket_urlpatterns = [
-    path('ws_todolist/data', consumers.MyConsumer.as_asgi()),
+    path('kanban/data', consumers.MyConsumer.as_asgi()),
 ]
