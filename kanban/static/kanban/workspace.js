9f144c4babbbaebed70faf63e578a9be3be6e060
Minhui Xie <minhuix@andrew.cmu.edu>
2023-04-17 03:46:03 -0400

    added edit task frontend

--- kanban/static/kanban/workspace.js
+++ kanban/static/kanban/workspace.js
@@ -4,42 +4,84 @@ const priority_levels = ['High', 'Medium', 'Low'];
 
 // Function to create a task element
 function createTaskElement(task) {
-    const taskWrapper = document.createElement('div');
-    taskWrapper.className = 'task-wrapper';
+    var task_id = 0;
+    if (!task.fields.id) {
+      task_id = task.pk
+    } else {
+      task_id = task.fields.id;
+    }
+    // modal button that contains task overview
+    const taskWrapper = document.createElement("button");
+    taskWrapper.type = "button";
+    taskWrapper.id = `task-wrapper-${task_id}`;
+    taskWrapper.classList.add("task-wrapper");
+    taskWrapper.dataset.bsToggle = "modal";
+    taskWrapper.dataset.bsTarget = `#task-edit-modal`;
 
+    // task name header
     const taskTitle = document.createElement('h4');
     taskTitle.className = 'heading-m task-title';
     taskTitle.textContent = task.fields.taskname;
-
+    // task information wrapper
     const taskInfo = document.createElement('div');
     taskInfo.className = 'task-info';
-
+    // task sprint
     const taskSprintTag = document.createElement('div');
-    taskSprintTag.className = 'task-tag';
+    taskSprintTag.className = 'task-tag task-sprint';
     taskSprintTag.textContent = 'Sprint ' + task.fields.sprint;
-
+    // task priority
     const taskPriorityTag = document.createElement('div');
     taskPriorityTag.className = 'task-tag';
     taskPriorityTag.textContent = 'Priority: ';
-
+    // task priority
     const taskPriorityValue = document.createElement('div');
     taskPriorityValue.className = 'task-priority-' + priority_levels[task.fields.priority - 1].toLowerCase();
     taskPriorityValue.textContent = priority_levels[task.fields.priority - 1]
 
     taskPriorityTag.appendChild(taskPriorityValue);
-
+    // task assignee
     const taskAssignee = document.createElement('div');
     taskAssignee.className = 'task-assignee';
     taskAssignee.textContent = task.fields.assignee_name;
 
+    // assembly task info wrapper
     taskInfo.appendChild(taskSprintTag);
     taskInfo.appendChild(taskPriorityTag);
     taskInfo.appendChild(taskAssignee);
-
+    // assembly task button
     taskWrapper.appendChild(taskTitle);
     taskWrapper.appendChild(taskInfo);
 
-    return taskWrapper;
+
+    // create event listener on clicking the button
+    // populate modal with task data
+    taskWrapper.addEventListener("click", function() {
+      let field_workspace = document.querySelector("#task-edit-modal #id_workspace");
+      field_workspace.value = task.fields.workspace;
+      field_workspace.disabled = true;
+      let field_taskname = document.querySelector("#task-edit-modal #id_taskname_input_text");
+      field_taskname.value = task.fields.taskname;
+      let field_description = document.querySelector("#task-edit-modal #id_description_input_text");
+      field_description.value = task.fields.description;
+      let field_assignee = document.querySelector("#task-edit-modal #id_assignee");
+      field_assignee.value = task.fields.assignee;
+      let field_creation_date = document.querySelector("#task-edit-modal #id_creation_date_input_date");
+      field_creation_date.disabled = true;
+      field_creation_date.value = task.fields["creation_date"];
+      let field_due_date = document.querySelector("#task-edit-modal #id_due_date_input_date");
+      field_due_date.value = task.fields["due_date"];
+      let field_status = document.querySelector("#task-edit-modal #id_status_input_select");
+      field_status.value = task.fields.status;
+      let field_sprint = document.querySelector("#task-edit-modal #id_sprint");
+      field_sprint.value = task.fields.sprint;
+      let field_priority = document.querySelector("#task-edit-modal #id_priority_input_select");
+      field_priority.value = task.fields.priority;
+
+      // hidden display of task id
+      let id = document.querySelector("#task-edit-modal .modal-task-id");
+      id.textContent = task_id
+    });
+  return taskWrapper;
 }
 
 
@@ -63,13 +105,22 @@ function compareTasks(a, b, sortBy) {
 // Paramater tasks is a list of tasks
 function updateTasks(tasks) {
   // Update HTML and put updated task to correct location
-  tasks.forEach(task => {
+  tasks.forEach(async task => {
     // locate assignee collapsible and column by assignee and status
-    let column = document.querySelector(`#collapse-body-${task.assignee} #${statusDict[task.status]}-column`);
-    let assigneeNameBtn = document.querySelector(`#collapse-btn-${task.assignee}`);
+    let column = document.querySelector(`#collapse-body-${task.assignee} #${statusDict[task.status]}-column`)
+    let assigneeNameBtn = document.querySelector(`#collapse-btn-${task.assignee}`)
 
-    // create tasks with matching structure to use createTaskElement
+    // create column if no assignee column is created
+    // TODO: handle the case when a column is not created 
+
+
+    // otherwise create tasks with matching structure to use createTaskElement
     if (column && assigneeNameBtn) {
+      // update task if the task is found and located
+      const taskElement = document.querySelector(`#task-wrapper-${task.id}`);
+      if (taskElement) {
+        taskElement.remove();
+      }
       let taskBody = {};
       taskBody['fields'] = task;
       taskBody['fields']['assignee_name'] = assigneeNameBtn.childNodes[0].data;
@@ -316,34 +367,13 @@ function createAssigneeColumn(assignee) {
       const columnsDiv = document.getElementById('columns-div');
       var count = 0;
       for (const name in groupedTasks) {
-        // create the bootstrap collapsible <a> element
-        const aElement = document.createElement("a");
-        aElement.classList.add("btn", "collapsed", "bg-primary-subtle", "workspace-collapsible-button");
-        aElement.setAttribute("id", `collapse-btn-${groupedTasks[name][0].fields.assignee}`);
-        aElement.setAttribute("data-bs-toggle", "collapse");
-        aElement.setAttribute("href", `#collapse-body-${groupedTasks[name][0].fields.assignee}`);
-        aElement.setAttribute("role", "button");
-        aElement.setAttribute("aria-expanded", "false");
-        aElement.setAttribute("aria-controls", "collapseExample");
-        aElement.textContent = name;
-
-        // create the <div> element with class "collapse"
-        const divCollapse = document.createElement("div");
-        divCollapse.classList.add("collapse", "show", "grouped-columns-wrapper");
-        divCollapse.setAttribute("id", `collapse-body-${groupedTasks[name][0].fields.assignee}`);
-
-        // append the <a> element and the <div> element with class "collapse" to task board
-        columnsDiv.appendChild(aElement);
-        columnsDiv.appendChild(divCollapse);
-
-        // Create the status columns and append them to the columns div
-        const statusColumns = statusList.map(createStatusColumn);
-        const collapsibleBody = document.getElementById(`collapse-body-${groupedTasks[name][0].fields.assignee}`);
-        statusColumns.forEach(column => collapsibleBody.appendChild(column));
-
+        // create basic framework to place tasks
+        // including <a> element, collapse, and status columns
+        createCollapse(name, groupedTasks[name][0].fields.assignee, columnsDiv);
         // Arrange tasks by status (TODO, DOING & DONE) within collapsible groups
         groupedTasks[name].forEach(async (task) => {
           const taskElement = createTaskElement(task);
+          const collapsibleBody = document.getElementById(`collapse-body-${groupedTasks[name][0].fields.assignee}`);
           const column = collapsibleBody.querySelector(`#${statusDict[task.fields.status]}-column`);
           column.appendChild(taskElement);
         });
@@ -351,6 +381,35 @@ function createAssigneeColumn(assignee) {
       }
     });
     
+  }
+
+  // create bootstrap collapse assignee column and the stratus columns inside
+  function createCollapse(name, assignee, columnsDiv) {
+    // create the bootstrap collapsible <a> element
+    const aElement = document.createElement("a");
+    aElement.classList.add("btn", "collapsed", "bg-primary-subtle", "workspace-collapsible-button");
+    aElement.setAttribute("id", `collapse-btn-${assignee}`);
+    aElement.setAttribute("data-bs-toggle", "collapse");
+    aElement.setAttribute("href", `#collapse-body-${assignee}`);
+    aElement.setAttribute("role", "button");
+    aElement.setAttribute("aria-expanded", "false");
+    aElement.setAttribute("aria-controls", "collapseExample");
+    aElement.textContent = name;
+
+    // create the <div> element with class "collapse"
+    const divCollapse = document.createElement("div");
+    divCollapse.classList.add("collapse", "show", "grouped-columns-wrapper");
+    divCollapse.setAttribute("id", `collapse-body-${assignee}`);
+
+    // append the <a> element and the <div> element with class "collapse" to task board
+    columnsDiv.appendChild(aElement);
+    columnsDiv.appendChild(divCollapse);
+
+    // Create the status columns and append them to the columns div
+    const statusColumns = statusList.map(createStatusColumn);
+    const collapsibleBody = document.getElementById(`collapse-body-${assignee}`);
+    statusColumns.forEach(column => collapsibleBody.appendChild(column));
+
   }
   
   // Function to toggle between the two sets of columns
